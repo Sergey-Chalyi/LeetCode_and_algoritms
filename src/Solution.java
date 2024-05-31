bade66b1f74e2143495a0fea85c39c0c3bd7f8ab
Sergey Chalyi <supersupergray@gmail.com>
2024-05-31 15:51:55 +0300

    my step

--- src/Solution.java
+++ src/Solution.java
@@ -13,8 +13,10 @@ class Solution {
 
     }
     public List<Integer> spiralOrder(int[][] matrix) {
+        //создаем лист с результатами
         List<Integer> result = new ArrayList<>();
 
+        //пока в листе есть эллементы выполням цикл
         while(countOfPositionsInMatrix(matrix) > 0) {
 
             //добавляем в лист верхний слой
@@ -25,24 +27,26 @@ class Solution {
             matrix = rotateAndCut(matrix);
 
         }
-        //result.add(matrix[0][0]);
         return result;
     }
 
     public int[][] rotateAndCut(int[][] matrix) {
+        //создаем лист, где будем хранить последовательность чисел после среза верхнего слоя
         List<Integer> tempList = new ArrayList<>();
+        //записываем исходные габариты матирицы
         int high = matrix.length;
         int width = matrix[0].length;
 
+        //основная логика последовательности записи чисел во временный лист (тут на картинке нужно объяснять)
         for(int countOfIterationsHigh = width - 1; countOfIterationsHigh >= 0; countOfIterationsHigh--) {
             for(int countOfIterationsWidth = 1; countOfIterationsWidth <= high - 1; countOfIterationsWidth++) {
                 tempList.add(matrix[countOfIterationsWidth][countOfIterationsHigh]);
             }
-        }//nen все ок
+        }
 
-        //создаем новую матрицу
+        //создаем новую матрицу с новой высотой и шириной (почему именно такие высота и ширина - нужно рисовать)
         int[][] newMatrix = new int[width][high - 1];
-        //и заполняем ее значениями
+        //и заполняем ее значениями просто последовательно из листа, ибо в листе числа уже в нужной нам последовательности расположены
         int indexForTempList = 0;
         for(int i = 0; i < newMatrix.length; i++) {
             for(int j = 0; j < newMatrix[0].length; j++) {
@@ -50,11 +54,11 @@ class Solution {
                 indexForTempList++;
             }
         }
-        //присваиваем исходной матрице новую матрицу
         return newMatrix;
     }
 
     private int countOfPositionsInMatrix(int[][] matrix) {
+        //тупо подсчет
         int count = 0;
         for(int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
