05c8a9fc17979b43b3eb4d6deff56facc497dff1
Sergey Chalyi <supersupergray@gmail.com>
2024-06-10 10:05:45 +0300

    main laptop

---
+++ src/LeetCode_Solved/EASY/E_ReverseVowelsOfAString_345.java
@@ -0,0 +1,42 @@
+package LeetCode_Solved.EASY;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+//10-06-2024
+public class E_ReverseVowelsOfAString_345 {
+    public static void main(String[] args) {
+        System.out.println(new E_ReverseVowelsOfAString_345().reverseVowels("hello"));
+    }
+    public String reverseVowels(String string) {
+        // cоздаем скроку, в которую будем записывать финальный результат
+        String resultString = "";
+        // cоздаем лист для хранения всех гласных из слова
+        List<Character> characters = new ArrayList<>();
+        // заполняем его
+        for(int i = 0; i < string.length(); i++) {
+            if (isAVoteSymbol(string.charAt(i))) characters.add(string.charAt(i));
+        }
+        // переворачиваем лист
+        Collections.reverse(characters);
+        //теперь опять проходим циклом по исходной строке и заменяем все символы
+        for(int i = 0; i < string.length(); i++) {
+            if (isAVoteSymbol(string.charAt(i))) { // если буква - гласная, то добавляем в результат с заменой
+                resultString += characters.getFirst(); // добавляем первый символ из листа (развернутого) с буквами
+                characters.removeFirst(); // и удаляем первый елемент листа
+            } else {
+                resultString += string.charAt(i); // в противном случае просто добавляем согласную к результату
+            }
+        }
+        return resultString;
+
+    }
+
+    private boolean isAVoteSymbol(char symbol) {
+        Character[] vowels = {'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'};
+        List<Character> listWithVoteCharacters = Arrays.asList(vowels);
+        // проверяем, находится ли символ в промежутке английских букв нижнего и верхнего регистра
+        return listWithVoteCharacters.contains(symbol);
+    }
+}
